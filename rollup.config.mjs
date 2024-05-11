24cbb54962534e00fb92c28cdfe0f4ee4bcaa065
Mr Stone <pierre.evens16@gmail.com>
2024-05-11 18:32:26 -0400

    Normalize

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import multi from '@rollup/plugin-multi-entry'
 import commonjs from '@rollup/plugin-commonjs'
@@ -14,23 +13,11 @@ const inputs = {
 export default Object.entries(inputs).map(([name, input]) => ({
 	input,
 	output: [
-    { format: 'es', file: `dist/${name}.mjs` },
-    { format: 'cjs', file: `dist/${name}.cjs` }
+    { format: 'es', file: `dist/${name}.js` }
   ],
-  onwarn: (warning, handler) => {
-    if (
-      ['CIRCULAR_DEPENDENCY', 'THIS_IS_UNDEFINED'].includes(warning.code) &&
-      [].concat(warning.id, warning.ids).filter(v => v?.includes('/node_modules/')).length // Disable for node_modules
-    ) {
-      return
-    } else {
-      handler(warning)
-    }
-  },
   plugins: [
-    json(),
     multi(),
-    nodeExternals({ deps: false }), // Must always be before `nodeResolve()`.
+    nodeExternals(), // Must always be before `nodeResolve()`.
     nodeResolve({
       exportConditions: ['node', 'import', 'require', 'default']
     }),
